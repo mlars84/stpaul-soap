31c940c318978d558c1a02a9676464934eee41c3
Matt Larson <matt.a.larson@gmail.com>
2017-08-18 08:18:53 -0500

    adding in styled-components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import Header from './components/Header'
 import Footer from './components/Footer'
 import './App.css'
+import styled from 'styled-components';
+const Button = styled.button`
+  border-radius: 3px;
+  padding: 0.25em 1em;
+  margin: 0 1em;
+  background: palevioletred;
+  color: white;
+  border: 2px solid palevioletred;
+`
+
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Header />
+      <div className="App-header">
+        <h1>Saint Paul Soap</h1><Button>Button</Button>
         <Footer />
       </div>
     );
---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Route, IndexRoute} from 'react-router'
+import Template from '../containers/Template'
+import Home from '../containers/Home'
+
+
+const createRoutes = () => {
+  return (
+    <Route
+      path='/'
+      component={Template}
+    >
+      <IndexRoute
+        component={Home}
+      />
+    </Route>
+  )
+}
+
+const Routes = createRoutes()
+
+export default Routes
