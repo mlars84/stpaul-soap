9b8292c400e02a04a69b8f989d9762c9cdbe07dd
Matt Larson <matt.a.larson@gmail.com>
2017-08-31 10:45:44 -0500

    added appbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import { Top } from '../styled/Header'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar'
 
 class Header extends React.Component {
   render() {
@@ -8,6 +10,12 @@ class Header extends React.Component {
         <div>
           <h4>Saint Paul Soap</h4>
         </div>
+        <MuiThemeProvider>
+          <AppBar
+            title="Nav"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+          />
+        </MuiThemeProvider>
       </Top>
     );
   }
