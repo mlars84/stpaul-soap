3c06a21617f1d98964275608c6b8ec64c47ed556
Matt Larson <matt.a.larson@gmail.com>
2017-09-06 11:40:53 -0500

    adding state and props in

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React from 'react'
 import Home from './containers/Home'
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      txt: 'this is state',
+      cat: 0
+    }
+  }
+  update( e ){
+    this.setState({ txt: e.target.value })
+  }
   render() {
     return (
       <div>
         <Home />
+        <Widget update={this.update.bind(this)} />
+        <h1>{this.state.txt} - {this.state.cat}</h1>
       </div>
-    );
+    )
   }
 }
+
+const Widget = (props) =>
+<input type="text" onChange={props.update} />
 
 export default App;
