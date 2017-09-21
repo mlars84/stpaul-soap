aa147c95141817cdf634c16c83c57a61dfa7bc6a
Matt Larson <matt.a.larson@gmail.com>
2017-09-20 21:38:48 -0500

    adding in sass and animate.css

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import { All, Header, testImg } from '../styled/Home'
 import Footer from './Footer'
 import styledBody from '../styled/Home'
+// import LoadAssets from 'react-asset-loader'
 
 class Home extends Component {
   render() {
@@ -10,17 +11,19 @@ class Home extends Component {
       <MuiThemeProvider>
         <All>
           <Header>
+            <h1 class="animated infinite bounce">Example</h1>
             StPaulSoap.COM
           </Header>
           <styledBody className="styledBody">
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
+            {/* <LoadAssets assets={[{"uri":"/static/images/IMG_3629.JPG"}]} /> */}
           </styledBody>
         </All>
       </MuiThemeProvider>
     )
   }
-}
+}//end Home
 
 export default Home
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import styles from './theme/global.css'
+import styles from './theme/global.scss'
 
 
 ReactDOM.render(
